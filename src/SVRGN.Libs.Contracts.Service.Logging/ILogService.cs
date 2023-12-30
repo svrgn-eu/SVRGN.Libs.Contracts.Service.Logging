82f1658fa602d30dcf710b389bd8361f2f2c3d31
efilnukefesin <efil.threat@googlemail.com>
2023-12-30 23:01:29 +0100

    first commit

---
+++ src/SVRGN.Libs.Contracts.Service.Logging/ILogService.cs
@@ -0,0 +1,35 @@
+﻿using SVRGN.Libs.Contracts.Service.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SVRGN.Libs.Contracts.Service.Logging
+{
+    /// <summary>
+    /// this service is responsible for logging and escalating Errors and Fatals to the ErrorService
+    /// </summary>
+    public interface ILogService : IService
+    {
+        #region Properties
+
+        int FatalCount { get; }
+        int ErrorCount { get; }
+        int WarningCount { get; }
+        int InfoCount { get; }
+        int DebugCount { get; }
+        int TotalCount { get; }
+
+        #endregion Properties
+
+        #region Methods
+
+        void Fatal(string SenderClassName, string SenderMethodName, string Entry, Exception exception = null);
+        void Error(string SenderClassName, string SenderMethodName, string Entry, Exception exception = null);
+        void Warning(string SenderClassName, string SenderMethodName, string Entry, Exception exception = null);
+        void Info(string SenderClassName, string SenderMethodName, string Entry, Exception exception = null);
+        void Debug(string SenderClassName, string SenderMethodName, string Entry, Exception exception = null);
+        void SetMinimumLogLevel(string LogLevel);
+
+        #endregion Methods
+    }
+}
---
+++ src/SVRGN.Libs.Contracts.Service.Logging/ILogStrategy.cs
@@ -0,0 +1,19 @@
+﻿using SVRGN.Libs.Contracts.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SVRGN.Libs.Contracts.Service.Logging
+{
+    public interface ILogStrategy : IBaseObject
+    {
+        #region Properties
+
+        #endregion Properties
+
+        #region Methods
+        void Output(string Severity, string SenderClassName, string SenderMethodName, string Entry, bool ShallIncludeTimeStamps, Exception Exception);
+
+        #endregion Methods
+    }
+}
